6cad06ad9f846c1a226cca3cb95cd8fe7dad10b4
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-19 02:10:02 +0900

    변수와 데이터 입력

---
+++ 3-10.c
@@ -0,0 +1,13 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int age; // 나이는 정수형
+	double height; // 키는 실수형 
+	printf("Enter age and height: "); // 입력하세요
+	scanf("%d%lf", &age, &height); // 나이와 키를 입력
+	printf("age is %d , height is %.1lfcm\n", age, height);
+	return 0;
+}
---
+++ 3-11.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	char grade;
+	char name[20]; // 학점을 입력할 변수
+
+	printf("학점 입력 : "); 
+	scanf("%c", &grade);
+
+	printf("이름 입력 : "); 
+	scanf("%s", name);
+	printf("%s 의 학점은 %c 입니다 . \n", name, grade);
+
+	return 0;
+}
