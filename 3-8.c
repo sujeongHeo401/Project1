d8e58a91f11857c37a4ebf815311a5a04a3a79e5
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-19 01:09:33 +0900

    use const

---
+++ 3-8.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int income = 0; // 소득액 초기화
+	double tax;  // 세금
+	const double tax_rate = 0.12; /// 세율 초기화 
+
+	income = 456; // 소득액 저장
+	tax = income * tax_rate;  // 세금 계싼
+	printf("tax is : %.1lf\n", tax);
+
+	return 0;
+	
+}
