7fddd3a47edab740d4d5a8928e8ae86d07a9ff8c
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-19 02:35:10 +0900

    4-4.c prefix, postfix calculate

---
+++ 4-1.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+	int sum, sub, mul, inv;
+
+	a = 10;
+	b = 20;
+	sum = a + b;
+	sub = a - b;
+	mul = a * b;
+	inv = -a; // 음수 연산 후 대입 연산
+	printf("a의 값 :%d, b의 값 : %d\n", a, b);
+	printf("덧셈: %d\n", sum);
+	printf("뺄셈: %d\n", sub);
+	printf("곱셈: %d\n", mul);
+	printf("a의 음수 연산 : %d\n", inv);
+
+	return 0;
+}
---
+++ 4-2.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	double apple; // 실수
+	int banana; // 정수
+	int orange; // 정수
+
+	apple = 5.0 / 2.0; // 실수와 실수의 나누기 연산 
+	banana = 5 / 2;
+	orange = 5 % 2; 
+
+	printf("apple : %.1lf\n", apple);
+	printf("banana : %d\n", banana);
+	printf("orange : %d\n", orange);
+
+	return 0;
+}
