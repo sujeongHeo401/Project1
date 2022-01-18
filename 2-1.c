ab0ef61ceb79b3b333441c9f93b0a45b07037098
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-18 21:52:07 +0900

    Add project files.

---
+++ 2-1.c
@@ -0,0 +1,15 @@
+﻿/* 작성자: 홍길동
+	제목: 10과 20을 더하는 프로그램 */
+#include <stdio.h>
+int main(void)
+{
+	printf("%d\n", 12); // 10진수 정수 상수 출력
+	printf("%d\n", 014); // 10진수 정수 상수 출력
+	printf("%d\n", 0xc); // 10진수 정수 상수 출력
+	printf("%o\n", 12); // 16 진수 12진수로 
+	printf("%x\n", 12);
+	printf("%X\n", 12);
+
+	return 0;
+
+}
