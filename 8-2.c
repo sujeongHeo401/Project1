42feb009cb84b83f63d14f0925c8ab59d8fa9834
sujeongHeo401 <gotnwjd40@gmail.com>
2022-01-19 14:02:52 +0900

    배열을 처리해부자....

---
+++ 8-2.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int score[5]; // 다섯 과목의 성적을 입력한 int 형 배열 선언 
+	int i;  // 반복 제어 변수
+	int total = 0; // 총점을 누적할 변수 
+	double avg; // 평균을 저장하는 변수
+
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%d", &score[i]);
+	}
+
+	for (i = 0; i < 5; i++) {
+		total += score[i];
+	}
+	
+	avg = total / 5.0;
+
+	for (i = 0; i < 5; i++) {
+		printf("%5d", score[i]);
+	}
+
+	printf("\n"); 
+	printf("avg : %.1lf\n", avg); /// 평균 출력 
+
+	return 0;
+}
---
+++ 8-3.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int score[5];
+
+	int i;
+	int total = 0;
+	double avg;
+	int count;       // 배열요소의 개수를 계산
+
+	count = sizeof(score) / sizeof(score[0]); /// 배열 요소의 개수 계산 
+	for (i = 0; i < count; i++) {
+		scanf_s("%d", &score[i]);
+	}
+
+	for (i = 0; i < count; i++) {
+		total += score[i];
+	}
+	avg = total / (double)count; //총합을 count 로 나누어 평균 계산
+
+	for (i = 0; i < count; i++) {
+		printf("%5d", score[i]);
+	}
+	printf("\n");
+
+	printf("average : %.1f\n", avg); 
+	return 0;
+}
