7a62f44204b105da5c51f3e19d57256a4a0c2138
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-02 13:47:08 +0900

    허용되지 않는 포인터 대입

---
+++ 9-6.c
@@ -0,0 +1,16 @@
+﻿///Pointer type which not allowed
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10; // init variable
+	int* p = &a;
+	double* pd; // 
+
+	pd = p; // put p into pd
+	printf("%lf\n", *pd); // print value that pd pointing . 
+
+	return 0;
+
+
+}
