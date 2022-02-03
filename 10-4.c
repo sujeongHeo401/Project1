3b86e375dbc8bf3de503534a77fdf7e2dc380bc5
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-03 13:13:25 +0900

    작업

---
+++ 10-4.c
@@ -0,0 +1,21 @@
+﻿// 포인터 뺄셈 연산
+#include <stdio.h>
+
+int main(void)
+{
+	int ary[5] = { 10, 20, 30, 40, 50 };
+	int* pa = ary; // 첫 번째 배열 요소 주소
+	int* pb = pa + 3; // 네 번째 배열 요소 주소
+
+	printf("pa : %u\n", pa);
+	printf("pb : %u\n", pb);
+
+	pa++; // pa 를 다음 배열 요소로 이동 
+	printf("pb - pa : %u/n", pb - pa); // 두 포인터 뺄셈
+	printf("앞에 있는 배열 요소의 값 출력 : ");
+	if (pa < pb) printf("%d\n", *pa); // pa 가 배열의 앞에 있으면 *pa 출력
+	else printf("%d\n", *pb);
+
+	return 0;
+
+}
