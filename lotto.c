825255db7ee8b0a98922e990eca6d3226d8e42ea
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-03 14:41:50 +0900

    배열 연습 > ㄹ또 번호 입력 받구(배열) 출력(배열)

---
+++ lotto.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+void input_nums(int* lotto_nums);
+void print_nums(int* lotto_nums);
+
+int main(void)
+{
+	int lotto_nums[6];
+
+	input_nums(lotto_nums); // 입력 함수 호출
+	print_nums(lotto_nums);
+	return 0;
+}
+
+void input_nums(int* lotto_nums)
+{
+	int num;
+	int i, j;
+	int dup;
+	for (i = 0; i < 6; i++)
+	{
+		printf("enter number : ");
+		scanf_s("%d", &num);
+		dup = 0;
+		for (j = 0; j < i; j++)
+		{
+			if (num == lotto_nums[j])
+			{
+				dup = 1;
+				break;
+			}
+		}
+		if (!dup)
+		{
+			lotto_nums[i] = num;
+		}
+		else
+		{
+			printf("already has same number!\n");
+			i--;
+		}
+	}
+}
+
+void print_nums(int* lotto_nums)
+{
+	int i;
+	printf("로또 번호 : ");
+	for (i = 0; i < 6; i++)
+	{
+		printf("%d ", lotto_nums[i]);
+	}
+}
