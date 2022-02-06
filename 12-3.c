6b77ac6915ebfc99f6d8c420db45bedfa782698a
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-06 21:31:53 +0900

    문자열 공부중

---
+++ 12-3.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	char str[80];
+
+	printf("enter character array : ");
+	scanf("%s", str);
+	printf("enter first word : %s \n", str); // print the string of array 
+	scanf("%s", str);
+	printf("second word that in buffer %s\n", str);
+
+	return 0;
+}
