2c874c88d21ce0ba66831dfd6d3811699c7d00bc
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-20 20:57:10 +0900

    string

---
+++ 12-11.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <string.h> // strlen 함수 사용을
+
+int main(void)
+{
+	char str1[80], str2[80];
+	char* resp; // 문자열이 긴 배열을 선택할 포인터
+
+	printf("Enter two file name : ");
+
+	scanf("%s%s", str1, str2); // 배열에 입력된 문자열의 길이 비교
+	if (strlen(str1) > strlen(str2))
+		resp = str1;
+	else
+		resp = str2;
+
+	printf("File name that the name is longer ?? %s\n", resp);
+
+	return 0;
+}
